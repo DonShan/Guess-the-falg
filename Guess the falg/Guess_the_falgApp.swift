6b8dd12665128acc2097c88b4d79a5e5c222ec3a
DonShan <msdjshan47@gmail.com>
2022-12-08 17:02:56 +0530

    Initial Commit

---
+++ Guess the falg/Guess_the_falgApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_falgApp.swift
+//  Guess the falg
+//
+//  Created by Madushan Senavirathna on 2022-12-08.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_falgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
